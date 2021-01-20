fd2e92d8487ba247e6bb7253699d5f8164051798
Rao Usama Bin Sohail <raousamabinsohail@gmail.com>
2021-01-20 23:27:48 +0500

    2nd

--- canvasDraw.js
+++ canvasDraw.js
@@ -1,21 +1,35 @@
+window.onload =  resizeCanvas()
 
+document.getElementById("sheet").addEventListener("click", function () {
+  console.log("sssssssssss=>", "click");
+  window.pencolor = "white";
+});
 
-document.getElementById("eraser-canvas").addEventListener("click",function(){
-  console.log("sssssssssss=>","click")
-  window.pencolor = "white"
-})
+function resizeCanvas() {
+  console.log(window.innerHeight, "------->", window.innerWidth);
+  var convas = document.getElementById("sheet");
 
+  convas.width = window.innerWidth/3;
 
+  
+}
 
-document.getElementById("eraser-canvas-2").addEventListener("click",function(){
-  console.log("sssssssssss=>","click")
-  window.pencolor = "white"
-})
+document.getElementById("eraser-canvas").addEventListener("click", function () {
+  console.log("sssssssssss=>", "click");
+  window.pencolor = "white";
+});
 
-document.getElementById("black1").addEventListener("click",function(){
-  window.pencolor = "black"
-})
+document
+  .getElementById("eraser-canvas-2")
+  .addEventListener("click", function () {
+    console.log("sssssssssss=>", "click");
+    window.pencolor = "white";
+  });
 
-document.getElementById("black2").addEventListener("click",function(){
-  window.pencolor = "black"
-})
+document.getElementById("black1").addEventListener("click", function () {
+  window.pencolor = "black";
+});
+
+document.getElementById("black2").addEventListener("click", function () {
+  window.pencolor = "black";
+});
